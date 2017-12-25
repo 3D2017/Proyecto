ceae5d421edf53e09dbb966d252576bf9052d360
Andrés Leone <forestrf@gmail.com>
2017-12-25 18:04:18 +0100

    qr + vuforia funcionando

--- Proyecto 3D/Assets/TestQR.cs
+++ Proyecto 3D/Assets/TestQR.cs
@@ -1,40 +1,97 @@
 ﻿using System;
 using UnityEngine;
+using Vuforia;
 using ZXing;
 
 public class TestQR : MonoBehaviour {
-	public Texture2D marker;
-	private WebCamTexture camTexture;
-	private Rect screenRect;
+	public UserDefinedTargetBuildingBehaviour targetBuilder;
+	public float minAreaForUserTargetCreation = .3f;
+	public bool debug = false;
+	public Texture2D debugMarker;
+
+	float currentArea;
+	Vector2 camSize;
+	ResultPoint[] points = new ResultPoint[0];
+	IBarcodeReader barcodeReader = new BarcodeReader();
+	Image.PIXEL_FORMAT mPixelFormat = Image.PIXEL_FORMAT.RGBA8888;
+	bool mFormatRegistered = false;
 
 	void Start() {
-		//screenRect = new Rect(0, 0, Screen.width, Screen.height);
-		camTexture = new WebCamTexture();
-		//camTexture.requestedHeight = Screen.height;
-		//camTexture.requestedWidth = Screen.width;
-		if (camTexture != null) {
-			camTexture.Play();
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+	}
+
+	void OnVuforiaStarted() {
+		if (CameraDevice.Instance.SetFrameFormat(mPixelFormat, true)) {
+			Debug.Log("Successfully registered pixel format " + mPixelFormat.ToString());
+			mFormatRegistered = true;
+		} else {
+			Debug.LogError(
+				"Failed to register pixel format " + mPixelFormat.ToString() +
+				"\n the format may be unsupported by your device;" +
+				"\n consider using a different pixel format.");
+			mFormatRegistered = false;
 		}
 	}
 
-	void OnGUI() {
-		// drawing the camera on screen
-		GUI.DrawTexture(new Rect(0, 0, camTexture.width, camTexture.height), camTexture, ScaleMode.ScaleToFit);
-		// do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+	void Update() {
+		if (!mFormatRegistered) return;
+		
+		Image image = CameraDevice.Instance.GetCameraImage(mPixelFormat);
+
 		try {
-			IBarcodeReader barcodeReader = new BarcodeReader();
 			// decode the current frame
-			var result = barcodeReader.Decode(camTexture.GetPixels32(),
-			  camTexture.width, camTexture.height);
+			camSize = new Vector2(image.Width, image.Height);
+			var result = barcodeReader.Decode(image.Pixels, image.Width, image.Height, RGBLuminanceSource.BitmapFormat.RGBA32);
 			if (result != null) {
 				Debug.Log("DECODED TEXT FROM QR: " + result.Text);
-				var points = result.ResultPoints;
-				for (int i = 0; i < points.Length; i++) {
-					GUI.DrawTexture(new Rect(points[i].X - marker.width / 2f, points[i].Y - marker.height / 2f, marker.width, marker.height), marker);
+				points = result.ResultPoints;
+				Rect r = GetPercentageRect(points);
+				currentArea = r.width * r.height;
+				if (currentArea >= minAreaForUserTargetCreation) {
+					targetBuilder.BuildNewTarget(result.Text, 1);
+
+					// download model
+
+					enabled = false;
 				}
 			}
-
 		}
 		catch (Exception ex) { Debug.LogWarning(ex.Message); }
+	}
+
+	void OnGUI() {
+		if (!mFormatRegistered) return;
+		if (!debug) return;
+
+		for (int i = 0; i < points.Length; i++) {
+			GUI.DrawTexture(new Rect(
+				points[i].X / camSize.x * Screen.width - debugMarker.width / 2f,
+				points[i].Y / camSize.y * Screen.height - debugMarker.height / 2f, 
+				debugMarker.width, debugMarker.height), debugMarker);
+		}
+
+		Rect r = GetPercentageRect(points);
+		r.x *= Screen.width;
+		r.y *= Screen.height;
+		r.width *= Screen.width;
+		r.height *= Screen.height;
+		GUI.Box(r, currentArea + " < " + minAreaForUserTargetCreation);
+	}
+
+	public Rect GetPercentageRect(ResultPoint[] points) {
+		Vector4 minmax = new Vector4(9999999, 9999999, 0, 0);
+		for (int i = 0; i < points.Length; i++) {
+			minmax.x = Math.Min(minmax.x, points[i].X);
+			minmax.y = Math.Min(minmax.y, points[i].Y);
+			minmax.z = Math.Max(minmax.z, points[i].X);
+			minmax.w = Math.Max(minmax.w, points[i].Y);
+		}
+
+		minmax.x /= camSize.x;
+		minmax.y /= camSize.y;
+		minmax.z /= camSize.x;
+		minmax.w /= camSize.y;
+
+		return new Rect(minmax.x, minmax.y, minmax.z - minmax.x, minmax.w - minmax.y);
 	}
 }
